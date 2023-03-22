2df2077439e67b9b0277f57cd525825693aaf37a
imran_khan_41 <imran.khan@tkxel.io>
2023-03-22 11:16:06 +0500

    routing initial comment

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  faLock = faLock;
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor( private router: Router) {}
+
+  ngOnInit(): void {
+
+  }
+  onSubmit(): void {
+
+  }
+}
